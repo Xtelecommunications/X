424fbca0fa0008aa74caa0a18dbb3909b325d6f3
Xtelecommunications <telecommunicationsx@gmail.com>
2024-05-29 21:58:02 +0200

    Update connect.js

--- connect.js
+++ connect.js
@@ -4,13 +4,13 @@
 ░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
 
 
-* @project_name PRABATH-MD
-* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+* @project_name 𝕏
+* @authors CraigeeX and Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
 * @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
 * @link <https://github.com/prabathLK/PRABATH-MD>
 * @version 2.0.6
 
-© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+© 2024 Prabath Kumara, SACHI-BOT. All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
